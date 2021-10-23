264f03adc47e96c059dcaa992602f95f01aca2fa
Anushree Abhyankar <anush.abhyankar@gmail.com>
2021-10-22 21:20:23 -0400

    Update add to bag fn to support multiple pgs

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const addItemToCart = () => {
     const selectedQty = document.getElementById("a5-bun-quantity");
-    console.log(selectedQty.options[selectedQty.selectedIndex].text)
-
     const selectedGlaze = document.getElementById("a5-bun-glaze");
-    console.log(selectedGlaze.options[selectedGlaze.selectedIndex].text)
 
     const selectedItem = {
+        "type": JSON.parse(window.localStorage.getItem("currentPageDetails")).name,
         "qty": selectedQty.options[selectedQty.selectedIndex].text,
         "glaze": selectedGlaze.options[selectedGlaze.selectedIndex].text
     }
 
-    window.localStorage.setItem('item', JSON.stringify(selectedItem));
-
-    console.log(JSON.parse(window.localStorage.getItem('item')))
+    const currentBagContent = JSON.parse(window.localStorage.getItem('bagContent')) || [];
+    const newBagContent = [...currentBagContent, selectedItem]
+    window.localStorage.setItem('bagContent', JSON.stringify(newBagContent));
 
     updateBagCount(window.localStorage.length)
 }
